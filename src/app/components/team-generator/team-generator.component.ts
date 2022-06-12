1cb34a50f331101ca6982000fcc6fed5f141074d
SAUD ALNASSER <saud4services@gmail.com>
2022-06-12 06:13:22 +0300

    Add the ability to add members to be split into teams

--- src/app/components/team-generator/team-generator.component.ts
+++ src/app/components/team-generator/team-generator.component.ts
@@ -5,4 +5,29 @@ import { Component } from '@angular/core';
   templateUrl: './team-generator.component.html',
   styleUrls: ['./team-generator.component.scss'],
 })
-export class TeamGeneratorComponent {}
+export class TeamGeneratorComponent {
+  protected members: string[];
+  protected newMember: string;
+  protected errorMessage: string;
+
+  public constructor() {
+    this.members = [];
+    this.newMember = '';
+    this.errorMessage = '';
+  }
+
+  protected onMemberInput(newMember: string): void {
+    this.newMember = newMember;
+  }
+
+  protected onAddMemberButtonClick(): void {
+    if (!this.newMember) {
+      this.errorMessage = "Name can't be empty.";
+      return;
+    }
+
+    this.members.push(this.newMember);
+    this.newMember = '';
+    this.errorMessage = '';
+  }
+}
